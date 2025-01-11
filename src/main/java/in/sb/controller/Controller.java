353771384bb04edc1d61a120df8fd310bde24d3f
nthokal24 <nthokal3@gmail.com>
2025-01-11 14:37:56 +0530

    Test

---
+++ src/main/java/in/sb/controller/Controller.java
@@ -0,0 +1,25 @@
+package in.sb.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+	
+	
+	@GetMapping("/show")
+	public String show() {
+		
+		logger.info("Show Method Started");
+		logger.info("Show Method Test");
+		logger.info("Show Method Spring boot");
+		logger.info("Show Method Java");
+			return "Hello Logger";
+	}
+	
+	
+}
